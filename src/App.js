2d31dc0d3b632c82f9a938232b84f33af576e809
Artak Harutyunyan <artak_harutyunyan@mail.com>
2022-04-17 13:54:19 +0400

    Setting up board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React, { useState, createContext } from 'react';
+import './App.css';
+
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import { boardDefault } from './Words';
+
+export const AppContext = createContext();
+
 function App() {
-  return <div></div>;
+  const [board, setBoard] = useState(boardDefault);
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Keyboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Keyboard() {
+  return <div>Keyboard</div>;
+}
+
+export default Keyboard;
